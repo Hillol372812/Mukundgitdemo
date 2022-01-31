14a97d6fa63042e5294cde4b652ac10d57436a65
Hillol372812 <58452314+Hillol372812@users.noreply.github.com>
2022-01-31 19:30:39 +0530

    Update Dropdownhandle.java

--- src/test/java/com/selenium4/test/Selenium4implementation/Dropdownhandle.java
+++ src/test/java/com/selenium4/test/Selenium4implementation/Dropdownhandle.java
@@ -20,7 +20,7 @@ public class Dropdownhandle {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-
+//line added by hillol
 
 		WebElement Option=driver.findElement(By.id("option"));
 
